523b561ca4d8beda07cdbeaccb0869999c9ceabf
someia2026 <someia.ouaziz@gmail.com>
2025-08-05 16:53:49 +0200

    commit

---
+++ zoo.py
@@ -0,0 +1,11 @@
+# Class Animal:
+#     Poids = ""
+#     Taille = ""
+#     def se_deplacer() #méthode
+#         pass
+#     def 
+
+# Oiseau = Animal()
+# Mammifere = Animal()
+# Reptile = Animal()
+# Amphibien = Animal()
